c38c4fd0b85dfff676e29742ea24113ec47732d4
Carlos Asaed Reyes Vazquez <carlosasaedreyesvazquez@gmail.com>
2020-07-21 04:30:25 -0500

    Cambios
    Cambios y arreglo de errores

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: ['./home.css'],
+
 })
 export class HomeComponent implements OnInit {
 
--- src/app/components/works/works.component.ts
+++ src/app/components/works/works.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-works',
-  templateUrl: './works.component.html',
-  styleUrls: ['./works.component.css']
+  templateUrl: './works.component.html'
 })
 export class WorksComponent implements OnInit {
 
